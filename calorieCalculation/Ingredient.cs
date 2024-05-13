8726eade3c7d8db558b13341be1ae7e00840440b
RC_Student_lab <RC_Student_lab@labG9AEB3>
2024-05-13 12:42:00 +0000

    Add project files.

---
+++ calorieCalculation/Ingredient.cs
@@ -0,0 +1,8 @@
+﻿namespace RecipeApplicationTests
+{
+    internal class Ingredient
+    {
+        public string Name { get; set; }
+        public int Calories { get; set; }
+    }
+}
---
+++ calorieCalculation/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("calorieCalculation")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("calorieCalculation")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("33724fbb-08b9-4b23-9e1a-8429390157de")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
